187e9b1c922ec6bc8834355a6cadccb1130bdd9c
Michael Haan <mhaan@opentable.com>
2016-03-25 13:36:50 -0700

    removed folders from repo

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/bluebird/index.d.ts" />
-/// <reference path="browser/ambient/node-uuid/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/require/index.d.ts" />
-/// <reference path="browser/definitions/node-uuid/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/bluebird/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/require/index.d.ts" />
-/// <reference path="main/definitions/node-uuid/index.d.ts" />
